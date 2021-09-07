52b0b2119c51eb6b19a8bce1e39ad02165a889fd
huibang <amiablee@163.com>
2021-09-07 22:08:35 +0800

    提交了demo-https

---
+++ demo-https/src/main/java/com/example/demohttps/DemoHttpsApplication.java
@@ -0,0 +1,16 @@
+package com.example.demohttps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot+ https
+ */
+@SpringBootApplication
+public class DemoHttpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHttpsApplication.class, args);
+	}
+
+}
